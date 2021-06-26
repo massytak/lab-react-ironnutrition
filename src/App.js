3f5b4c7e2c72b7072de7045e48da2e407cb928cf
massytak <massi.messalti@gmail.com>
2021-06-26 16:58:45 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './foodbox/FoodBox';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <FoodBox name={foods[0].name} calories={foods[0].calories} image={foods[0].image} />
+     <FoodBox name={foods[1].name} calories={foods[1].calories} image={foods[1].image} />
+     <FoodBox name={foods[2].name} calories={foods[2].calories} image={foods[2].image} />
+     <FoodBox name={foods[3].name} calories={foods[3].calories} image={foods[3].image} />
+     <FoodBox name={foods[4].name} calories={foods[4].calories} image={foods[4].image} />
+     <FoodBox name={foods[5].name} calories={foods[5].calories} image={foods[5].image} />
     </div>
   );
 }
